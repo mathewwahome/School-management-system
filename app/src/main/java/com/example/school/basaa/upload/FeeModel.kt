7efc8d2d2aab7a3bd57126bc6ef8a9bdd5cbd0cc
wa2f <mathewwahome2003@gmail.com>
2022-09-01 10:15:48 +0300

    new

---
+++ app/src/main/java/com/example/school/basaa/upload/FeeModel.kt
@@ -0,0 +1,10 @@
+package com.example.school.basaa.upload
+
+data class FeeModel(
+    var stdname :String?=null,
+    var adm:String?=null,
+    var date :String?=null,
+    var amount :String?=null,
+    var totalfee:String?=null,
+    var method:String?=null
+)
